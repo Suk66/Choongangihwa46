ef6e45f888cfcdb3282d8b875553f9d9259d0500
Suk66 <goawwl66@gmail.com>
2025-04-01 16:31:52 +0900

    Initial commit

---
+++ duoprojectv1/src/main/java/com/wecompany/duoprojectv1/Duoprojectv1Application.java
@@ -0,0 +1,13 @@
+package com.wecompany.duoprojectv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Duoprojectv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Duoprojectv1Application.class, args);
+    }
+
+}
---
+++ duoprojectv1/src/test/java/com/wecompany/duoprojectv1/Duoprojectv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecompany.duoprojectv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Duoprojectv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
